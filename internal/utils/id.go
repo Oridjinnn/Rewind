5c60ab99eb2a5a92aeb54c39d71ff9221d8ea7fb
habeldavidson007-glitch <habeldavidson007@gmail.com>
2026-05-20 00:18:46 +0700

    fix session id collision using nanosecond entropy

--- internal/utils/id.go
+++ internal/utils/id.go
@@ -1,13 +1,32 @@
 package utils
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"time"
 )
 
 func GenerateSessionID() string {
+
+	timestamp := time.Now().UnixNano()
+
+	randomBytes := make([]byte, 3)
+
+	_, err := rand.Read(randomBytes)
+
+	if err != nil {
+		return fmt.Sprintf(
+			"session_%d",
+			timestamp,
+		)
+	}
+
+	randomPart := hex.EncodeToString(randomBytes)
+
 	return fmt.Sprintf(
-		"session_%d",
-		time.Now().Unix(),
+		"session_%d_%s",
+		timestamp,
+		randomPart,
 	)
 }
